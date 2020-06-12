389f7d173608dcc15cfd29a35fd10ed82a5da3d2
Hai <65085715+bqhai@users.noreply.github.com>
2020-06-12 23:21:33 +0700

    Create project QuanLyQuanCafe
    design usercontrol

---
+++ QuanLyQuanCafe/QuanLyQuanCafe/frmLogin.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucLogin1 = new ThuVienControl.ucLogin();
+            this.SuspendLayout();
+            // 
+            // ucLogin1
+            // 
+            this.ucLogin1.Location = new System.Drawing.Point(12, 14);
+            this.ucLogin1.Name = "ucLogin1";
+            this.ucLogin1.Size = new System.Drawing.Size(474, 245);
+            this.ucLogin1.TabIndex = 0;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 271);
+            this.Controls.Add(this.ucLogin1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ThuVienControl.ucLogin ucLogin1;
+    }
+}
+
